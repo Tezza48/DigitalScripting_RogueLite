f6293cb505f52d3cb474e4c35e3c0077a227f4b0
Daniel Hamilton <Daniel Hamilton>
2016-04-28 16:50:08 +0100

    Recommitting files to the build

---
+++ Assets/Scr/NPCFollow.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+var minDistance = 10;
+var maxDistance = 50;
+var moveSpeed = 5;
+var Player : Transform;
+
+function Start(){
+	//if (Player == null)
+	//	Player = Transform.FindWithTag ("Player");
+}
+
+
+function Update()
+{
+
+		if (Vector3.Distance(transform.position, Player.position) >= minDistance);
+		{
+			transform.position += transform.forward * moveSpeed * Time.deltaTime;
+		}	
+			
+			if(Vector3.Distance(transform.position, Player.position) <= maxDistance)
+				{
+		
+		}
+	}
+
+
