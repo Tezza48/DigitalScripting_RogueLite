0665c9d0dc678a0c97660e3eeb863091fbca21f5
Samuel Cook <samuel.cook51@gmail.com>
2016-04-28 15:56:17 +0100

    bullet script added

---
+++ Assets/Scr/bullet.js
@@ -0,0 +1,21 @@
+#pragma strict
+
+public var Bullet: GameObject;
+public var speed = 20;
+
+function Start ()
+{
+if (Bullet == null)
+	Bullet = GameObject.FindWithTag ("Bullet")
+}
+
+function Update ()
+{
+	if (Input.GetButtonDown("Fire1")
+	{
+	var clone : Rigidbody;
+		clone = Instantiate (Bullet, transform.position, transform.rotation);
+		
+		clone.velocity = transform.TransformDirection (Vector3.forward * speed);
+	}
+}
