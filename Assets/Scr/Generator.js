3eab3f014bc2c9d5c776db53475c6dd9ea2589ad
William Terry <Tezza48is@aol.com>
2016-04-28 20:12:48 +0100

    Generator finished, added a material
    texture is from textures.com as per usual.

--- Assets/Scr/Generator.js
+++ Assets/Scr/Generator.js
@@ -23,31 +23,71 @@ function Start ()
 			cells[x,y] = 1;
 		}
 	}
+
+	DrunkenWalk();
 }
 
 function DrunkenWalk()
 {
 	var clearedCells = 0;
-	var startPosition : Vector2 = new Vector2(Random.Range(1, width), Random.Range(1, height));
-	var lastposition : Vector2 = startPosition;
-
+	var startPosition : Vector2 = new Vector2(Random.Range(1, width-1), Random.Range(1, height-1));
+	var lastPosition : Vector2 = startPosition;
+	
 	cells[startPosition.x, startPosition.y] = 0;
 
-	while(clearedCells < cellsToClear)
+	var tries = 100;
+	while(clearedCells < cellsToClear && tries < 10000)
 	{
-		var currentCell = cells[lastposition.x, lastposition.y];
-		if(currentCell == 1)
+		//pick the position to walk in
+		var direction;
+		var currentPosition : Vector2;
+		var isValid = false;
+		while(!isValid)
 		{
-			currentCell = 0;
+			direction = Random.Range(0, 4);
+			switch (direction)
+			{
+				case 0:
+					currentPosition = new Vector2(lastPosition.x, lastPosition.y + 1);
+					isValid = (currentPosition.y < height - 1);
+					break;
+				case 1:
+					currentPosition = new Vector2(lastPosition.x + 1, lastPosition.y);
+					isValid = (currentPosition.x < width - 1);
+					break;
+				case 2:
+					currentPosition = new Vector2(lastPosition.x, lastPosition.y - 1);
+					isValid = (currentPosition.y > 0);
+					break;
+				case 3:
+					currentPosition = new Vector2(lastPosition.x - 1, lastPosition.y);
+					isValid = (currentPosition.x > 0);
+					break;
+			}
+		}
+		
+		if(cells[currentPosition.x, currentPosition.y] == 1)
+		{
+			cells[currentPosition.x, currentPosition.y] = 0;
 			clearedCells++;
 		}
+		lastPosition = currentPosition;
+		tries++;
 	}
 
 	for(var y = 0; y < height; y++)
 	{
 		for(var x = 0; x < width; x++)
 		{
-
+			var currentCell = cells[x, y];
+			switch (currentCell) {
+				case 0:
+					Instantiate(FloorPrefab, new Vector3(x, 0, y), Quaternion.identity);
+					break;
+				case 1:
+					Instantiate(WallPrefab, new Vector3(x, 0, y), Quaternion.identity);
+					break;
+			}
 		}
 	}
 }
