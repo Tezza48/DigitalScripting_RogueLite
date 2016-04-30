94a9de22cc2d96c86e7ee3abd59e435532492b11
William Terry <Tezza48@users.noreply.github.com>
2016-04-30 22:32:01 +0100

    Simplified movement

--- Assets/Scr/Movement.js
+++ Assets/Scr/Movement.js
@@ -6,15 +6,18 @@ public var Shot : GameObject;
 
 
 
-function Update(){
-
-var pos = Camera.main.WorldToScreenPoint(transform.position); 
-var dir = Input.mousePosition - pos; 								// Follows the mouse and positions it towards it.
-var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;  
-transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); 
-
-var translation : float = Input.GetAxis ("Horizontal") * speed;
-var rotation : float = Input.GetAxis ("Vertical") * rotationSpeed;
+function Update()
+{
+	var newPos = new Vector3(Input.GetAxix("Horizontal"), 0f, Input.GetAxix("Vertical"));
+	transform.position += newPos * Time.deltaTime * speed;
+/*
+	var pos = Camera.main.WorldToScreenPoint(transform.position); 
+	var dir = Input.mousePosition - pos; 								// Follows the mouse and positions it towards it.
+	var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;  
+	transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); 
+	
+	var translation : float = Input.GetAxis ("Horizontal") * speed;
+	var rotation : float = Input.GetAxis ("Vertical") * rotationSpeed;
 
 	translation *= Time.deltaTime;
 	rotation *= Time.deltaTime;
@@ -22,8 +25,8 @@ var rotation : float = Input.GetAxis ("Vertical") * rotationSpeed;
 	transform.Translate (0,0, translation);
 	transform.Translate (0, rotation, 0);
 
-
-	}
+*/
+}
 
 if (Input.GetMouseButtonDown(0)){
 		var newShot:GameObject = Instantiate(Shot, transform.position, transform.rotation) as GameObject;
