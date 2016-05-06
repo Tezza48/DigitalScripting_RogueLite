e3141fc79b152a2779995929b579a532c4366d07
Samuel Cook <samuel.cook51@gmail.com>
2016-05-06 02:48:34 +0100

    main menu scene

---
+++ Assets/Scripts/menu/instructions.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+public var _anim : Animator;
+
+function Start () 
+{
+_anim = GetComponent.<Animator>();
+}
+
+public function SlideIn ()
+{
+	_anim.SetTrigger("slide in");
+}
+
+public function SlideOut ()
+{
+	_anim.SetTrigger("slide out");
+}
